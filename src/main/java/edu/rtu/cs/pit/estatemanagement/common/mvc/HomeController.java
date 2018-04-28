73cda05ffed9148e148dc59aaef91b371b497d98
dnk89 <dmitrij.kozlovskij@gmail.com>
2018-04-28 15:00:49 +0300

    Index page with login form

---
+++ src/main/java/edu/rtu/cs/pit/estatemanagement/common/mvc/HomeController.java
@@ -0,0 +1,18 @@
+package edu.rtu.cs.pit.estatemanagement.common.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
