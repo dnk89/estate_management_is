e49ec80d90f6708202f0b12bae58ceca70f9e693
dnk89 <dmitrij.kozlovskij@gmail.com>
2018-04-28 13:49:19 +0300

    Spring boot init

---
+++ src/main/java/edu/rtu/cs/pit/estatemanagement/EstateManagementApplication.java
@@ -0,0 +1,12 @@
+package edu.rtu.cs.pit.estatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/rtu/cs/pit/estatemanagement/EstateManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.rtu.cs.pit.estatemanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstateManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
